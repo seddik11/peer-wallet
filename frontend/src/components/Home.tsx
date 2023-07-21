575960815e65eb47c85036acd74ff41e240d2cee
grmkris <kristjan.grm1@gmail.com>
2023-07-22 00:54:58 +0200

    wc

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,9 +1,15 @@
 import Navbar from "./Navbar";
+import { BurnerWallets } from "@/features/burner/BurnerWallets";
+import { WalletConnect } from "@/features/WalletConnect/WalletConnect";
+import { WalletConnectModals } from "@/features/WalletConnect/modals/WalletConnectModals";
 
 export default function Home() {
   return (
     <div className="App min-h-screen">
       <Navbar />
+      <BurnerWallets />
+      <WalletConnect />
+      <WalletConnectModals />
     </div>
   );
 }
