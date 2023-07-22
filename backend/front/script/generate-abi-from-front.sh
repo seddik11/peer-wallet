e1c96785aa6ab3331e6c2d1e01ce123aa8c61f75
raigal-r <raquel.raigal@outlook.es>
2023-07-22 11:39:35 +0200

    sismo working splited

---
+++ backend/front/script/generate-abi-from-front.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This script generates the ABI files for the contracts in the project.
+# It is intended to be run from the project root directory
+# the generated ABI will be used to call the contracts from the frontend
+mkdir -p ./../abi/
+touch ./../abi/MintGovernanceToken.json
+touch ./../abi/PeerGovernanceToken.json
+
+
+echo "{
+  \"abi\": $(forge inspect MintGovernanceToken abi)
+}" >| ./../abi/MintGovernanceToken.json
+
+echo "{
+  \"abi\": $(forge inspect PeerGovernanceToken abi)
+}" >| ./../abi/PeerGovernanceToken.json
